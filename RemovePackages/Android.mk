ca36608871cb0e43a9ccdd301dc50a59903af1e2
MrDeath404 <pisarekpatryk404@gmail.com>
2025-04-23 18:35:39 +0200

    NPhone2: Added Remove Package list

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,27 +5,19 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    Chrome \
-    Chrome-Stub \
-    Drive \
-    GoogleCamera \
-    Maps \
-    MyVerizonServices \
-    OBDM_Permissions \
-    obdm_stub \
-    OemDmTrigger \
-    OPScreenRecord \
-    Ornament \
-    SafetyHubPrebuilt \
-    ScribePrebuilt \
-    Showcase \
-    Snap \
-    SprintDM \
-    SprintHM \
-    VZWAPNLib \
-    VzwOmaTrigger \
-    YouTube \
-    YouTubeMusicPrebuilt
+    Aperture \
+    Jelly \
+    ExactCalculator \
+    Recorder \
+    Etar \
+    Camelot \
+    Twelve \
+    DeskClock \
+    Glimpse \
+    Dialer \
+    messaging \
+    Contacts
+LOCAL_UNINSTALLABL
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
