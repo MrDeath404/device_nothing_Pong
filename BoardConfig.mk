f8d2cdf754f12c380c8be3f6147815ae584d586b
MrDeath404 <pisarekpatryk404@gmail.com>
2025-04-25 15:24:08 +0200

    NPhone2: Added UFS and LZ4 zram algorithm

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -247,5 +247,10 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+# UFS
+SOONG_CONFIG_NAMESPACES += ufsbsg
+SOONG_CONFIG_ufsbsg += ufsframework
+SOONG_CONFIG_ufsbsg_ufsframework := bsg
+
 # Include the proprietary files BoardConfig.
 include vendor/nothing/Pong/BoardConfigVendor.mk
