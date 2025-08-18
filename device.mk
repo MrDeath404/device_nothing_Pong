ee40a7a047c80c490f876532ad1f3d762826e288
MrDeath404 <pisarekpatryk404@gmail.com>
2025-08-18 13:49:04 +0200

    Added AppStore (needed client for gapps)

--- device.mk
+++ device.mk
@@ -516,5 +516,8 @@ PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink 
 
+# App Store
+PRODUCT_PACKAGES += AppStore
+
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/nothing/Pong/Pong-vendor.mk)
