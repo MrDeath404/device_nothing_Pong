ca35c9490d7da723acfe5b26f7d5f9b48b727bfc
MrDeath404 <pisarekpatryk404@gmail.com>
2025-08-25 15:30:56 +0200

    Pong: Adapted for BlackIron

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Pong.mk
+    $(LOCAL_DIR)/blackiron_Pong.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Pong-user \
-    lineage_Pong-userdebug \
-    lineage_Pong-eng
+    blackiron_Pong-user \
+    blackiron_Pong-userdebug \
+    blackiron_Pong-eng
